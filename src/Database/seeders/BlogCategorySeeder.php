f209154098b8e49b0e1b55bec1e942f0f500db12
Mr.VACO <brute1904@gmail.com>
2023-06-30 00:38:50 +0500

    Added blog category seeder

---
+++ src/Database/seeders/BlogCategorySeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace MrVaco\NovaBlog\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use MrVaco\NovaBlog\Models\Category;
+use MrVaco\NovaStatusesManager\Classes\StatusClass;
+
+class BlogCategorySeeder extends Seeder
+{
+    protected array $categories = [
+        [
+            'name'       => 'Uncategorized',
+            'slug'       => 'uncategorized',
+            'creator_id' => 1,
+        ],
+        [
+            'name'       => 'News',
+            'slug'       => 'news',
+            'creator_id' => 1,
+        ],
+        [
+            'name'       => 'Articles',
+            'slug'       => 'articles',
+            'creator_id' => 1,
+        ],
+    ];
+    
+    public function run(): void
+    {
+        foreach ($this->categories as $item)
+        {
+            Category::query()->create(array_merge($item, [
+                'status' => StatusClass::ACTIVE()->id,
+            ]));
+        }
+    }
+}
