beea1c03889ac6bf1d3fdbfd99ca2fd88e8c0482
Mr.VACO <brute1904@gmail.com>
2023-07-03 19:39:35 +0500

    Added Posts relation

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -5,6 +5,7 @@ namespace MrVaco\NovaBlog\Models;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use MrVaco\NovaStatusesManager\Classes\StatusClass;
 
@@ -47,5 +48,10 @@ class Category extends Model
     public function updator(): HasOne
     {
         return $this->hasOne(User::class, 'id', 'updator_id');
+    }
+    
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class, 'category_id');
     }
 }
