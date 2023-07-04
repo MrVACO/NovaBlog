a6d2df16634c81db75afcbb54b5699f0eaa134b9
Mr.VACO <brute1904@gmail.com>
2023-07-04 21:19:49 +0500

    Added Post sorting by "published_at" field

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -52,6 +52,8 @@ class Category extends Model
     
     public function posts(): HasMany
     {
-        return $this->hasMany(Post::class, 'category_id');
+        return $this
+            ->hasMany(Post::class, 'category_id')
+            ->orderBy('published_at', 'desc');
     }
 }
