222375503ad4945a9c8ed179d724de9f2167f0b8
Mr.VACO <brute1904@gmail.com>
2023-11-24 01:29:45 +0500

    Updated API routes

--- routes/api.php
+++ routes/api.php
@@ -6,20 +6,22 @@ use MrVaco\NovaBlog\Http\Controllers\CategoryController;
 use MrVaco\NovaBlog\Http\Controllers\PostController;
 
 app('router')
+    ->name('categories.')
+    ->prefix('categories')
     ->controller(CategoryController::class)
-    ->prefix('category')
-    ->group(function()
+    ->group(static function()
     {
-        app('router')->get('list', 'list');
-        app('router')->get('{category:slug}', 'show');
-    });
-
-app('router')
-    ->controller(PostController::class)
-    ->prefix('post')
-    ->group(function()
-    {
-        app('router')->get('recommended', 'recommended');
-        app('router')->get('{category:slug}/recommended', 'recommendedPostsFromCategory');
-        app('router')->get('{category}/{slug}', 'show');
+        app('router')->get('', 'list')->name('index');
+        app('router')->get('{category:slug}', 'show')->name('show');
+        
+        app('router')
+            ->name('posts.')
+            ->prefix('{category:slug}')
+            ->controller(PostController::class)
+            ->group(static function()
+            {
+                app('router')->get('posts', 'posts')->name('index');
+                app('router')->get('recommended', 'recommended')->name('recommended');
+                app('router')->get('{post:slug}', 'show')->name('show')->scopeBindings();
+            });
     });
--- src/Http/Controllers/PostController.php
+++ src/Http/Controllers/PostController.php
@@ -12,39 +12,35 @@ use MrVaco\NovaStatusesManager\Classes\StatusClass;
 
 class PostController extends Controller
 {
-    public function show(Post $post, string $category, string $slug)
+    public function show(Category $category, Post $post)
     {
         $active = StatusClass::ACTIVE()->id;
         
-        $data = $post
-            ->isActive()
-            ->where('slug', $slug)
-            ->with('category')
-            ->firstOrFail();
+        abort_unless($category->status === $active && $post->status === $active, 404);
         
-        abort_unless($data->category->slug === $category && $data->category->status === $active, 404);
-        
-        return PostResource::make($data);
+        return PostResource::make($post);
     }
     
-    public function recommended(Post $post)
+    public function posts(Post $post, Category $category)
     {
-        $data = $post::query()
+        $data = $post
             ->isActive()
-            ->isRecommended()
+            ->inCategory($category->id)
+            ->orderByDesc('published_at')
             ->paginate(12);
         
         return PostResource::collection($data);
     }
     
-    public function recommendedPostsFromCategory(Category $category, Post $post)
+    public function recommended(Category $category, Post $post)
     {
         abort_unless($category->status === StatusClass::ACTIVE()->id, 404);
         
-        $data = $post::query()
+        $data = $post
             ->isActive()
             ->isRecommended()
             ->inCategory($category->id)
+            ->orderByDesc('published_at')
             ->paginate(12);
         
         return PostResource::collection($data);
